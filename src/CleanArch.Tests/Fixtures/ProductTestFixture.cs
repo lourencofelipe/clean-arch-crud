60c23748af1a6e939741a4d85c4e9fe241d948fe
lourencofelipe <felipe.pucsc@gmail.com>
2025-04-03 11:04:11 +1300

    Adding unit tests for operations.

---
+++ src/CleanArch.Tests/Fixtures/ProductTestFixture.cs
@@ -0,0 +1,52 @@
+﻿using CleanArch.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using CleanArch.Persistence.Context;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Tests.Fixtures;
+public class ProductTestFixture : IDisposable
+{
+    public Product MainProduct { get; private set; }
+    public ProductRepository ProductRepository { get; private set; }
+    public EfContext Context { get; private set; }
+
+    public ProductTestFixture()
+    {
+        ConfigureDatabase();
+    }
+
+    private void ConfigureDatabase()
+    {
+        var options = new DbContextOptionsBuilder<EfContext>()
+            .UseInMemoryDatabase(databaseName: $"TestDb_{Guid.NewGuid()}")
+            .Options;
+
+        Context = new EfContext(options);
+        ProductRepository = new ProductRepository(Context);
+
+        ResetDatabase();
+    }
+
+    public void ResetDatabase() 
+    {
+        Context.Database.EnsureDeleted();
+        Context.Database.EnsureCreated();
+
+        MainProduct = new Product
+        {
+            ID = Guid.NewGuid(),
+            Name = "Keyboard test product",
+            Price = 150
+        };
+
+        Context.Product.Add(MainProduct);
+        Context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        // Remove the DB after finish the test execution.
+        Context.Database.EnsureDeleted(); 
+        Context.Dispose();
+    }
+}
---
+++ src/CleanArch.Tests/Unit/Repositories/Product/ProductRepositoryTests.cs
@@ -0,0 +1,95 @@
+﻿using CleanArch.Tests.Fixtures;
+using System.ComponentModel;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArch.Tests.Unit.Repositories.Product;
+
+public class ProductRepositoryTests : IClassFixture<ProductTestFixture>
+{
+    private readonly ProductTestFixture _fixture;
+    private static readonly Guid _productId = Guid.NewGuid();
+
+    public ProductRepositoryTests(ProductTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    [Description("Should successfully create a new product in the database.")]
+    public void AddProduct_Should_AddProductToDataBase()
+    {
+        // Arrange
+        var product = new CleanArch.Domain.Entities.Product
+        {
+            ID = Guid.NewGuid(),
+            Name = "Laptop Test",
+            Price = 20
+        };
+
+        // Act
+        _fixture.ProductRepository.CreateProduct(product);
+        var newProduct = _fixture.Context.Product.Find(product.ID);
+
+        // Assert
+        newProduct.Should().NotBeNull();
+        newProduct.Name.Should().Be("Laptop Test");
+        newProduct.Price.Should().Be(20);
+    }
+
+    [Fact]
+    [Description("Should return a valid product based on ID.")]
+    public void GetProduct_Should_ReturnValidProductById()
+    {
+        // Arrange
+        _fixture.ResetDatabase();
+        var product = _fixture.MainProduct;
+
+        // Act
+        var retrievedProduct = _fixture.ProductRepository.GetById(product.ID);
+
+        // Assert
+        retrievedProduct.Should().NotBeNull();
+        retrievedProduct.Name.Should().Be(product.Name);
+    }
+
+    [Fact]
+    [Description("Should return updated product details.")]
+    public void UpdateProduct_Should_ModifyProductDetails()
+    {
+        // Arrange
+        _fixture.ResetDatabase();
+        var product = _fixture.MainProduct;
+
+        var productName = "New keyboard Name";
+        var productPrice = 100;
+
+        product.Name = productName;
+        product.Price = productPrice;
+
+        // Act
+        _fixture.ProductRepository.UpdateProduct(product);
+        var updatedProduct = _fixture.Context.Product.Find(product.ID);
+
+        // Assert
+        updatedProduct.Should().NotBeNull();
+        updatedProduct.Name.Should().Be(productName);
+        updatedProduct.Price.Should().Be(productPrice);
+    }
+
+    [Fact]
+    [Description("Should successfully remove the product from database.")]
+    public void RemoveProduct_Should_RemoveProductFromDataBase() 
+    {
+        // Arrange
+        _fixture.ResetDatabase();
+
+        // Act
+        _fixture.ProductRepository.RemoveProduct(_fixture.MainProduct.ID);
+        var removedProduct = _fixture.Context.Product.Find(_fixture.MainProduct.ID);
+
+        // Assert
+        removedProduct.Should().BeNull();
+    }
+}
+
